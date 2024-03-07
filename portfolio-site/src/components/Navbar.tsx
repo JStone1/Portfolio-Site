4e0ef65394410631c8da0aff6e7ccf653d0d1ee9
JStone1 <62293789+JStone1@users.noreply.github.com>
2024-03-07 17:14:12 +0000

    Added SCSS and Navbar component

---
+++ portfolio-site/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <>
+      <header className="navbar">
+        <h2>John Stone</h2>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
