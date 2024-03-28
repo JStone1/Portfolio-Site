d6dada8a7b660052ea6e866fc4cb0c3c60eac279
JStone1 <62293789+JStone1@users.noreply.github.com>
2024-03-28 17:41:50 +0000

    Added local storage for colour switch

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,25 +1,81 @@
 import "./ThemeSwitcher.scss";
+import { useRef, useEffect } from "react";
 
 interface Props {}
 
 function ThemeSwitcher({}: Props) {
+  const storeTheme = (theme: string) => {
+    localStorage.setItem("theme", theme);
+  };
+
+  const getTheme = () => {
+    const activeTheme = localStorage.getItem("theme");
+    if (greenInput?.current?.id === activeTheme) {
+      greenInput.current.checked = true;
+    } else if (pinkInput?.current?.id === activeTheme) {
+      pinkInput.current.checked = true;
+    }
+  };
+
+  const orangeInput = useRef<HTMLInputElement>(null);
+  const greenInput = useRef<HTMLInputElement>(null);
+  const pinkInput = useRef<HTMLInputElement>(null);
+
+  // "!" lets ts know that it will NOT be null
+  const handleOrangeColourClick = () => {
+    console.log(orangeInput?.current?.id);
+    storeTheme(orangeInput?.current?.id!);
+  };
+  const handleGreenColourClick = () => {
+    console.log(greenInput?.current?.id);
+    storeTheme(greenInput?.current?.id!);
+  };
+  const handlePinkColourClick = () => {
+    console.log(pinkInput?.current?.id);
+    storeTheme(pinkInput?.current?.id!);
+  };
+
+  // runs getTheme() when page loads similar to "componentDidMount()"
+  useEffect(() => {
+    getTheme();
+  }, []);
+
   return (
     <>
       <form className="theme-picker" action="">
         <fieldset>
           <legend className="visually-hidden">Pick theme</legend>
-          <label className="visually-hidden" htmlFor="theme">
+          <label className="visually-hidden" htmlFor="orange">
             Orange
           </label>
-          <input type="radio" name="theme" id="orange" defaultChecked />
-          <label className="visually-hidden" htmlFor="theme">
+          <input
+            type="radio"
+            name="theme"
+            id="orange"
+            defaultChecked
+            ref={orangeInput}
+            onClick={handleOrangeColourClick}
+          />
+          <label className="visually-hidden" htmlFor="green">
             Green
           </label>
-          <input type="radio" name="theme" id="green" />
-          <label className="visually-hidden" htmlFor="theme">
+          <input
+            type="radio"
+            name="theme"
+            id="green"
+            ref={greenInput}
+            onClick={handleGreenColourClick}
+          />
+          <label className="visually-hidden" htmlFor="pink">
             Pink
           </label>
-          <input type="radio" name="theme" id="pink" />
+          <input
+            type="radio"
+            name="theme"
+            id="pink"
+            ref={pinkInput}
+            onClick={handlePinkColourClick}
+          />
         </fieldset>
       </form>
     </>
