bd6307bdb1f321e1a49526966434537d5b8d930c
JStone1 <62293789+JStone1@users.noreply.github.com>
2024-05-20 16:00:32 +0100

    Modified logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import "./Header.scss";
 import Nav from "@components/Nav/Nav";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <header>
-        {/* <h2>John Stone</h2> */}
-        <img className="logo" src="/assets/jslogo.svg" />
+        <div className="logo-title-container">
+          <NavLink to="/">
+            <img className="logo" src="/assets/jslogo.svg" />
+          </NavLink>
+          <h2>John Stone</h2>
+        </div>
         <Nav />
       </header>
     </>
